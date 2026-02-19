404019a6f62ff5ffb5ca47cf074e403fabd6043c
brookewaltz24-sudo <brooke.waltz.24@cnu.edu>
2026-02-19 09:36:40 -0500

    Update StudentSignup.tsx

--- src/app/components/student/StudentSignup.tsx
+++ src/app/components/student/StudentSignup.tsx
@@ -4,12 +4,12 @@ import { Button } from '@/app/components/ui/button';
 import { Input } from '@/app/components/ui/input';
 import { Label } from '@/app/components/ui/label';
 import { Card } from '@/app/components/ui/card';
-import { AuthProivder } from '@/app/context/AuthContext';
+import { useAuth } from '@/app/context/AuthContext';
 import { ArrowLeft } from 'lucide-react';
 
 export function StudentSignup() {
   const navigate = useNavigate();
-  const { login } = AuthProvider();
+  const { login } = useAuth();
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
