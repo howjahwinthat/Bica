ab15c6967bdad8ecd56827882ae01665d3261349
brookewaltz24-sudo <brooke.waltz.24@cnu.edu>
2026-03-10 21:47:09 -0400

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
-// src/app/App.tsx
-import { RouterProvider } from "react-router-dom";
-import { router } from "@/app/routes";
-import { AuthProvider } from "@/app/context/AuthContext";
+import { RouterProvider } from "react-router";
+import { router } from "./routes";
+import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "./components/ui/sonner";
 
 export default function App() {
   return (
     <AuthProvider>
       <RouterProvider router={router} />
+      <Toaster />
     </AuthProvider>
   );
 }
