0dd382d4a1ab2a9922b120123f2125e327dd14b2
brookewaltz24-sudo <brooke.waltz.24@cnu.edu>
2026-03-10 21:24:38 -0400

    Update CreateTimeslot.tsx

--- src/app/components/admin/CreateTimeslot.tsx
+++ src/app/components/admin/CreateTimeslot.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { format, parseISO, isBefore, addMinutes } from "date-fns";
-import { useAuth } from "@/app/context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 interface Room {
   id: number;
